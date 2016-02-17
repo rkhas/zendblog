aa551bc6399c4f008f5695f27db55e926d7fe50c
rkhas <sglavbuhprof@gmail.com>
2016-02-17 12:32:32 +0300

    module,config.php создан

---
+++ module/MyBlog/config/module.config.php
@@ -0,0 +1,15 @@
+<?php
+
+'doctrine' => array(
+	'driver' => array(
+		'myblog_entity' => array(
+			'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+			'paths' => array(__DIR__ . '/../src/MyBlog/Entity')
+		),
+		'orm_default' => array(
+			'drivers' => array(
+				'MyBlog\Entity' => 'myblog_entity',
+			)
+		)
+	),
+);
